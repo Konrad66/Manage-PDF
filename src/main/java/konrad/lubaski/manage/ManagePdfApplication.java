d35e2a81b9c8fd3e1176ba7d7a892136dfa514bc
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2025-02-10 13:47:23 +0100

    initial version

---
+++ src/main/java/konrad/lubaski/manage/ManagePdfApplication.java
@@ -0,0 +1,13 @@
+package konrad.lubaski.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagePdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagePdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/konrad/lubaski/manage/ManagePdfApplicationTests.java
@@ -0,0 +1,13 @@
+package konrad.lubaski.manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagePdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
