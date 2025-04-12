f3b552362ff4caedd3c245dd1b87b931f92e1729
Konrad Lubaski <k.lubaski.sltzn@gmail.com>
2025-04-12 13:32:06 +0200

    refactor

--- src/main/java/konrad/lubaski/manage/common/DisableSpringSecurity.java
+++ src/main/java/konrad/lubaski/manage/common/DisableSpringSecurity.java
@@ -5,19 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
-import java.util.Scanner;
-
 @Component
 public class DisableSpringSecurity {
-
-    public DisableSpringSecurity() {
-        System.out.println("disable spring security");
-    }
-
-    @Bean
-    public Scanner createScanner() {
-        return new Scanner(System.in);
-    }
 
     @Bean
     public SecurityFilterChain configureSecurity(HttpSecurity httpSecurity) throws Exception {
--- src/main/java/konrad/lubaski/manage/pdf/DependencyInjectionDemo.java
+++ src/main/java/konrad/lubaski/manage/pdf/DependencyInjectionDemo.java
@@ -1,17 +1,12 @@
 package konrad.lubaski.manage.pdf;
 
 import konrad.lubaski.manage.common.DisableSpringSecurity;
-import org.springframework.stereotype.Component;
 
-@Component
 public class DependencyInjectionDemo {
-
 
     private DisableSpringSecurity disableSpringSecurity;
 
     public DependencyInjectionDemo(DisableSpringSecurity disableSpringSecurity) {
         this.disableSpringSecurity = disableSpringSecurity;
-
-        System.out.println("Created pdf service");
     }
 }
